475bfa51d740d35ea94f5efd00d6966c0fbe2027
Nguyễn Viết Tuấn <thedemontuan@gmail.com>
2024-06-15 15:37:22 +0700

    optimize post carousel

--- src/components/Post/carousel.tsx
+++ src/components/Post/carousel.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { FaCircleChevronLeft, FaCircleChevronRight } from "react-icons/fa6";
 
 export default function Carousel({
@@ -14,13 +14,13 @@ export default function Carousel({
   const [curr, setCurr] = useState(0);
 
   const prev = () => setCurr((curr) => (curr === 0 ? slides.length - 1 : curr - 1));
-  const next = () => setCurr((curr) => (curr === slides.length - 1 ? 0 : curr + 1));
+  const next = useCallback(() => setCurr((curr) => (curr === slides.length - 1 ? 0 : curr + 1)), [slides.length]);
 
   useEffect(() => {
     if (!autoSlide) return;
     const slideInterval = setInterval(next, autoSlideInterval);
     return () => clearInterval(slideInterval);
-  }, []);
+  }, [autoSlide, autoSlideInterval, next]);
 
   return (
     <div className="overflow-hidden relative">

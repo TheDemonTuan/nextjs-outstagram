d2e4a54a03cacf232ddd9d76e14599b8f38ff7d1
Nguyen Hoa Ninh Dan <dh52007049@student.stu.edu.vn>
2024-06-28 19:00:07 +0700

    Custom top loading bars

--- src/app/(private)/your_activity/layout.tsx
+++ src/app/(private)/your_activity/layout.tsx
@@ -9,9 +9,14 @@ export default function YourActivityLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <Card className="max-w-screen-2xl mx-[170px] mt-[30px] p-0 flex border h-[565px] w-[940px] rounded-none shadow-none">
-      <HeaderActivity />
-      <div className=" bg-white dark:bg-neutral-950 w-3/4">{children}</div>
-    </Card>
+    <>
+      <div className="h-1 bg-gradient-to-r from-[#27c4f5] via-[#a307ba] via-[#fd8d32] via-[#70c050] to-[#27c4f5] bg-[length:500%] animate-[LoadingBarProgress_2s_linear_infinite,LoadingBarEnter_.5s_ease-out] transform-origin-left w-full"></div>
+      <div className="fixed left-0 right-0 top-0 z-12"></div>
+
+      <Card className="max-w-screen-2xl mx-[170px] mt-[30px] p-0 flex border h-[565px] w-[940px] rounded-none shadow-none">
+        <HeaderActivity />
+        <div className=" bg-white dark:bg-neutral-950 w-3/4">{children}</div>
+      </Card>
+    </>
   );
 }

5178330ddb6ca6814fe4b7c06d0c337da4bdd50a
Nguyen Hoa Ninh Dan <dh52007049@student.stu.edu.vn>
2024-06-29 10:23:48 +0700

    Fix hiển thị thiếu màu cho loading top bar

--- src/app/(private)/your_activity/layout.tsx
+++ src/app/(private)/your_activity/layout.tsx
@@ -10,7 +10,7 @@ export default function YourActivityLayout({
 }>) {
   return (
     <>
-      <div className="h-1 bg-gradient-to-r from-[#27c4f5] via-[#a307ba] via-[#fd8d32] via-[#70c050] to-[#27c4f5] bg-[length:500%] animate-[LoadingBarProgress_2s_linear_infinite,LoadingBarEnter_.5s_ease-out] transform-origin-left w-full"></div>
+      <div className="h-1 bg-custom-gradient bg-[length:500%] animate-[LoadingBarProgress_2s_linear_infinite,LoadingBarEnter_.5s_ease-out] transform-origin-left w-full"></div>
       <div className="fixed left-0 right-0 top-0 z-12"></div>
 
       <Card className="max-w-screen-2xl mx-[170px] mt-[30px] p-0 flex border h-[565px] w-[940px] rounded-none shadow-none">

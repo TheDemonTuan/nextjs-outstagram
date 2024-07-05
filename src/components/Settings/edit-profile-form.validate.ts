20d8989b1d511a0a47cc771635455b7ea3d11358
Nguyen Hoa Ninh Dan <dh52007049@student.stu.edu.vn>
2024-07-05 09:08:40 +0700

    Giới hạn độ tuổi phải lớn hơn 13 T

--- src/components/Settings/edit-profile-form.validate.ts
+++ src/components/Settings/edit-profile-form.validate.ts
@@ -1,5 +1,18 @@
 import { z } from "zod";
 
+const isAtLeast13 = (date: Date) => {
+  const today = new Date();
+  const birthDate = new Date(date);
+  const age = today.getFullYear() - birthDate.getFullYear();
+  const monthDiff = today.getMonth() - birthDate.getMonth();
+  const dayDiff = today.getDate() - birthDate.getDate();
+  if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+    return age - 1;
+  }
+  return age;
+};
+
+
 export const EditProfileFormValidateSchema = z.object({
   bio: z.string({
   }).max(150, {
@@ -35,12 +48,15 @@ export const EditProfileFormValidateSchema = z.object({
     }).trim(),
   gender: z
     .string({
-      required_error: "Giới tính không được để trống.",
+      required_error: "Gender cannot be left blank.",
     })
     .trim(),
   birthday: z.date({
     required_error: "A date of birth is required.",
     invalid_type_error: "A date of birth is invalid.",
+  })
+  .refine((date) => isAtLeast13(date) >= 13, {
+    message: "You must be at least 13 years old.",
   }),
 });
 

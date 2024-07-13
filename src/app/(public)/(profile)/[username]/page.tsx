09e5faf280ff261ab1157830e26e3ee3e4c50ec9
Nguyen Hoa Ninh Dan <dh52007049@student.stu.edu.vn>
2024-07-14 05:33:38 +0700

    Xử lý private account

--- src/app/(public)/(profile)/[username]/page.tsx
+++ src/app/(public)/(profile)/[username]/page.tsx
@@ -9,13 +9,13 @@ import { toast } from "sonner";
 import { notFound, useSearchParams } from "next/navigation";
 import { FiBookmark } from "react-icons/fi";
 import ProfileStories from "@/components/Profile/profile-stories";
-import { UserProfileDocument, UserProfileQuery } from "@/gql/graphql";
+import { Friend, UserProfileDocument, UserProfileQuery } from "@/gql/graphql";
 import { graphQLClient } from "@/lib/graphql";
 import { useQuery } from "@tanstack/react-query";
 import { userKey } from "@/api/user";
-import { Skeleton } from "@nextui-org/react";
+import { Button, Skeleton } from "@nextui-org/react";
 import { ProfileSkeleton } from "@/components/skeletons";
-import { ReelsIcon, ReelsProfileIcon, VideoAddIcon } from "@/icons";
+import { PrivateAccountIcon, ReelsIcon, ReelsProfileIcon, VideoAddIcon } from "@/icons";
 import Reel from "@/components/Profile/reel";
 import Saved from "@/components/Profile/saved";
 import { useAuth } from "@/hooks/useAuth";
@@ -71,6 +71,14 @@ const ProfilePage = ({ params }: { params: { username: string } }) => {
     return <div>User not found</div>;
   }
 
+  const isViewerFriendOrOwner =
+    authData?.username === userProfileData.userProfile.username ||
+    userProfileData.userProfile.user?.friends?.some(
+      (friend) => friend?.to_user_info?.id === authData?.id || friend?.from_user_info?.id === authData?.id
+    );
+
+  const canViewPrivateProfile = userProfileData.userProfile.user?.is_private === false || isViewerFriendOrOwner;
+
   return (
     <div className="flex flex-col max-w-screen-lg mt-9 mx-28 mb-40">
       <ProfileInformation userProfile={userProfileData} />
@@ -78,23 +86,40 @@ const ProfilePage = ({ params }: { params: { username: string } }) => {
       <div className="w-full">
         <hr className="border-gray-300 mt-14 border-t mx-28" />
       </div>
-      <div className="flex justify-center gap-10 mb-1">
-        <button className={getClassNames("POSTS")} onClick={() => setActiveTab("POSTS")}>
-          <IoMdGrid size="19" />
-          POSTS
-        </button>
-        <button className={getClassNames("REELS")} onClick={() => setActiveTab("REELS")}>
-          <ReelsProfileIcon width={19} height={19} />
-          REELS
-        </button>
-        {authData?.username === userProfileData.userProfile.username && (
-          <button className={getClassNames("SAVED")} onClick={() => setActiveTab("SAVED")}>
-            <FiBookmark size={19} />
-            SAVED
+      {canViewPrivateProfile && (
+        <div className="flex justify-center gap-10 mb-1">
+          <button className={getClassNames("POSTS")} onClick={() => setActiveTab("POSTS")}>
+            <IoMdGrid size="19" />
+            POSTS
           </button>
-        )}
-      </div>
-      {renderActiveTabContent(activeTab, userProfileData)}
+          <button className={getClassNames("REELS")} onClick={() => setActiveTab("REELS")}>
+            <ReelsProfileIcon width={19} height={19} />
+            REELS
+          </button>
+          {authData?.username === userProfileData.userProfile.username && (
+            <button className={getClassNames("SAVED")} onClick={() => setActiveTab("SAVED")}>
+              <FiBookmark size={19} />
+              SAVED
+            </button>
+          )}
+        </div>
+      )}
+
+      {!canViewPrivateProfile && (
+        <div className="flex flex-col items-center justify-center gap-5 mb-1 my-6">
+          <div className="flex items-center space-x-3">
+            <PrivateAccountIcon />
+            <div className="flex flex-col">
+              <span className="text-base font-semibold ">This account is private</span>
+              <span className="text-sm text-[#737373]"> Follow to see their photos and videos.</span>
+            </div>
+          </div>
+          <Button className="bg-[#0095F6] hover:bg-[#1877F2] text-sm text-white font-semibold px-5 py-3" radius="sm">
+            Add Friend
+          </Button>
+        </div>
+      )}
+      {canViewPrivateProfile && renderActiveTabContent(activeTab, userProfileData)}
     </div>
   );
 };
--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1780,3 +1780,34 @@ export const TelegramShareIcon = (props: SVGProps<SVGAElement>) => {
     </svg>
   );
 };
+
+export const PrivateAccountIcon = (props: SVGProps<SVGAElement>) => {
+  return (
+    <svg
+      aria-label=""
+      className="x1lliihq x1n2onr6 x5n08af"
+      fill="currentColor"
+      height="48"
+      role="img"
+      viewBox="0 0 96 96"
+      width="48">
+      <title></title>
+      <circle
+        cx="48"
+        cy="48"
+        fill="none"
+        r="47"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"></circle>
+      <path
+        d="M60.931 70.001H35.065a5.036 5.036 0 0 1-5.068-5.004V46.005A5.036 5.036 0 0 1 35.065 41H60.93a5.035 5.035 0 0 1 5.066 5.004v18.992A5.035 5.035 0 0 1 60.93 70ZM37.999 39.996v-6.998a10 10 0 0 1 20 0v6.998"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"></path>
+    </svg>
+  );
+};
